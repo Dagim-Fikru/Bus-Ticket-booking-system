1ae8d1e8d25a6cd0eb9e25eb4665aa029f3dd41e
Dagim Fikru <dagimfikru@gmail.com>
2022-12-26 00:55:14 +0300

    database connection

--- loginSignup/urls.py
+++ loginSignup/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('',views.welcomePage, name='home'),
     path('loginPage/',views.login, name='login'),
-    path('signup/',views.signUp,name='signup')
+    path('signup/',views.signup, name='signup')
 ]
--- loginSignup/views.py
+++ loginSignup/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth.models import User, auth  
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from pam import authenticate
 
 # Create your views here.
 
@@ -10,7 +12,16 @@ def welcomePage(request):
 def login(request):
     return render(request,'loginPage.html')
     
-def signUp(request):
+def signup(request):
+    # form = UserCreationForm(request.POST)
+    # if form.is_valid():
+    #     form.save()
+    #     username = form.cleaned_data.get('username')
+    #     password = form.cleaned_data.get('password1')
+    #     user = authenticate(username=username, password=password)
+    #     login(request, user)
+    #     return redirect('home')
+    # return render(request, 'signup.html', {'form': form})
     if request.method == 'POST':
         username = request.POST['username']
         email = request.POST['email']
@@ -18,8 +29,8 @@ def signUp(request):
         password2 = request.POST['password2']
 
         user = User.objects.create_user(username=username,password=password1,email=email)
-        user.save()
+        user.save();
         print('user created')
-        return redirect('home')
+        return redirect('')
     else:
         return render(request, 'signupPage.html')
